ddfaf75e42933936029a055af112eafe051185e2
AJoelj84 <ajoelj84@gmail.com>
2023-05-08 19:54:55 -0400

    server code

---
+++ server.js
@@ -0,0 +1,28 @@
+const PORT = process.env.PORT || 3001;
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors:{
+  }
+});
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  // Listen for new user event
+  socket.on('new user', (username) => {
+    socket.username = username;
+    console.log(username + " has joined the chat.");
+  });
+
+  // Listen for chat message event
+  socket.on('chat message', (msg) => {
+    console.log(socket.username + ': ' + msg);
+    io.emit('chat message', { username: socket.username, message: msg });
+  });
+});
+chatserver.server.listen(PORT, () => {
+    console.log(`chat server listening on ${PORT}`);
+  });
