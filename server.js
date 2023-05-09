019cdafef1b6a4be2e6d42f098a5ca12670269a8
AJoelj84 <ajoelj84@gmail.com>
2023-05-08 20:33:55 -0400

    server

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors:{
-    origin: 'https://only-cheese.herokuapp.com/',
-    methods: ["GET", "POST"]
+    origin: "*",
+    // origin: 'https://only-cheese.herokuapp.com/',
+    // methods: ["GET", "POST"]
   }
 });
 io.on('connection', (socket) => {
